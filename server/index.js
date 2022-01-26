26dbc9a7d069b417d83909fe9d20655a7d46ac20
Vadzim_Yesman <vvyesman@gmail.com>
2022-01-26 14:36:15 -0800

    Added a header, an image and a link to html

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require("path")
 require("dotenv").config()
-const port = process.env.PORT||5000
+const port = process.env.PORT||process.env.SERVER_PORT
 
 const app = express()
 
