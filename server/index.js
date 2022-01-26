7a664ae4d525f43e3f159b97e0c6f31eacaa8e44
Vadzim_Yesman <vvyesman@gmail.com>
2022-01-26 15:34:42 -0800

    css

--- server/index.js
+++ server/index.js
@@ -12,8 +12,8 @@ app.get('/', (req,res)=>{
 app.use(express.json())
 
 app.use('/js', express.static(path.join(__dirname, 'public/main.js')))
-app.use('/styles', express.static(path.join(__dirname, '/styles.css')))
-app.use('/reset', express.static(path.join(__dirname, '/reset.css')))
+app.use('/styles', express.static(path.join(__dirname, '../styles.css')))
+app.use('/reset', express.static(path.join(__dirname, '../reset.css')))
 
 
 app.listen(port, ()=>{
